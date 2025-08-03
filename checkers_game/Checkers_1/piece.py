0d37ac72494609f1eb4bf685875a1e4577f360e5
dherya <nishubedi655@gmail.com>
2025-08-03 13:26:55 +0530

    initial commit

--- checkers_game/Checkers_1/piece.py
+++ checkers_game/Checkers_1/piece.py
@@ -1,10 +1,13 @@
+# from constants file it will get all things 
 from .constants import RED, WHITE, SQUARE_SIZE, GREY, CROWN
 import pygame
 
+# PIECE LOGIC 
 class Piece:
     PADDING = 15
     OUTLINE = 2
-
+    
+    # INIT
     def __init__(self, row, col, color):
         self.row = row
         self.col = col
@@ -13,14 +16,17 @@ class Piece:
         self.x = 0
         self.y = 0
         self.calc_pos()
-
+    
+    # position checking 
     def calc_pos(self):
         self.x = SQUARE_SIZE * self.col + SQUARE_SIZE // 2
         self.y = SQUARE_SIZE * self.row + SQUARE_SIZE // 2
-
+    
+    #FROM normal piece to king piece
     def make_king(self):
         self.king = True
     
+    # JUST DRAW 
     def draw(self, win):
         radius = SQUARE_SIZE//2 - self.PADDING
         pygame.draw.circle(win, GREY, (self.x, self.y), radius + self.OUTLINE)
